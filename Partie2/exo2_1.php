21b97de03d4a8f121d36d7920b84b8a24bb7113e
Bobin <bobinpierreandre@gmail.com>
2025-04-28 13:28:27 +0200

    Ajout correction 2_2

--- Partie2/exo2_1.php
+++ Partie2/exo2_1.php
@@ -1,36 +1,72 @@
 <h1>Exercice 2</h1>
 
-<p>Soit le tableau suivant :
-$capitales = 
-[
-    "France" => "Paris", 
-"Allemagne" => "Berlin", 
-"USA" => "Washington", 
-"Italie" => "Rome"
-];
-
-Réaliser un algorithme permettant d’afficher le tableau HTML trié par ordre 
-alphabétique du nom de pays, avec les noms de pays en majuscules.
-
-<h2>Résultat</h2>
-
-// Correction & Commentaire 
-
-ici c'est un tableau associatif càd un tableau 
-une clé et une valeur 
-Syntaxe : 
-clé => valeur , 
-Attention la clé doit etre unique 
-Autant la valeur peut etre multiple mais la clé est unique 
-
-
-2 facons de déclarer un tableau : 
-$capitales = array();
+<p>Soit le tableau suivant : <br>
 $capitales = [
     "France" => "Paris", 
     "Allemagne" => "Berlin", 
     "USA" => "Washington", 
     "Italie" => "Rome"
+];  <br>
+
+Réaliser un algorithme permettant d’afficher le tableau HTML trié par ordre 
+alphabétique du nom de pays, avec les noms de pays en majuscules.</p>
+
+<h2>Résultat</h2>
+
+  <!-- Correction & Commentaire  -->
+
+<?php
+
+// Ici c'est un tableau associatif, càd un tableau ave une clé et une valeur 
+// Syntaxe : $key => $valeur;
+//  la clé doit etre unique - La valeur peut etre multiple mais pas la clé
+// Autre facon de  rédiger un tableau : $capitales = array() - mais mtn on privilégie la seconde
+// les crochets, plus lisible et plus consensuel 
+
+$capitales = [
+    "France" => "Paris", 
+    "Allemagne" => "Berlin", 
+    "USA" => "Washington", 
+    "Italie" => "Rome",
 ];
 
-Mais privilégier les crochets et le noté ainsi c'est plus lisible <!DOCTYPE html>
+// Tableau associatif ci-dessus 
+
+echo "<ul>";
+foreach($capitales as $pays => $capitale) {
+    echo "<li>$pays a pour capitale : $capitale </li>"; 
+}
+echo "</ul>";
+
+echo afficherTableHTML($capitales);  // Nom de la fonction
+
+function afficherTableHTML($capitales) {
+    ksort($capitales); // Trier le tableau dans l'ordre alphabétique de A à Z sur la clé
+    $result = "<table border=1>
+                 <thead>
+                       <tr>
+                            <th>Pays</th>
+                            <th>Capitale</th>
+                        </tr>
+                </thead>
+            <tbody>";
+
+    foreach($capitales as $pays => $capitale) {
+        $result .= "<tr>
+                        <td>" . mb_strtoupper($pays). "</td>
+                        <td>" . ucfirst($capitale) . "</td>
+                    </tr>";
+    }
+    $result .= "</tbody></table>";
+    return $result;
+}
+
+
+
+
+
+
+
+
+
+
